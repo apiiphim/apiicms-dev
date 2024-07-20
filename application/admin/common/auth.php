5d41ec4a478b2d576ef0096e5e01e583a288bcf6
Hi <124592551+trongdatdev@users.noreply.github.com>
2024-07-20 18:09:27 +0700

    Sửa hiển thị trang chủ

--- application/admin/common/auth.php
+++ application/admin/common/auth.php
@@ -244,7 +244,7 @@ return array(
         '96' => array("show"=>1,'name' => '[APII] Cào KKphim', 'controller' => 'collect',		'action' => 'kkphim'),
         '97' => array("show"=>1,'name' => '[APII] Cào NguonC', 'controller' => 'collect',		'action' => 'nguonc'),
         '98' => array("show"=>1,'name' => '[APII] Cào 3Nguồn', 'controller' => 'collect',		'action' => 'apiiphim'),
-        '99' => array("show"=>1,'name' => '[APII] Cào Hoạt hình', 'controller' => 'collect',		'action' => 'apiiphim_hoathinh'),
+        '99' => array("show"=>1,'name' => '[APII] Cào Hoạt Hình', 'controller' => 'collect',		'action' => 'apiiphim_hoathinh'),
         '100' => array("show"=>1,'name' => '[OP] Cào OPhim', 'controller' => 'collect',		'action' => 'ophim_goc'),
         '101' => array("show"=>1,'name' => '[KK] Cào KKphim', 'controller' => 'collect',		'action' => 'kkphim_goc'),
         // '96' => array("show"=>1,'name' => 'Myanimelist', 'controller' => 'collect',		'action' => 'myanimelist'),
